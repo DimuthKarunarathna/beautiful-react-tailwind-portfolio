00f29ce6243c01f901f8ad796bd67f42e2ec77e8
DimuthK <dimuthnova2i@gmail.com>
2025-05-26 19:51:59 +0530

    cv chnage

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -28,7 +28,7 @@ export const AboutSection = () => {
               </a>
 
               <a
-                href="/projects/Dimuth%20Karunarathna%20CV%20.pdf%20(1).pdf"
+                href="/public/projects/DimuthKarunarathnaCV.pdf"
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
                 download
               >
